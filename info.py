4d3724d6895c4e3a4e5ac477c6ff70bb08d403cf
vishnuu123pl <137207623+vishnuu123pl@users.noreply.github.com>
2023-10-15 16:40:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,7 +100,7 @@ APPROVED = environ.get("APPROVED_WELCOME", "off").lower()
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 # Set to False inside the bracket if you don't want to use Request Channel else set it to Channel ID
-REQ_CHANNEL = environ.get("REQ_CHANNEL", '-1001777460598')
+REQ_CHANNEL = environ.get("REQ_CHANNEL", False)
 REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
